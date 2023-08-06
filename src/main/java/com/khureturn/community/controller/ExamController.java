ad21ac5389909e427320b1764f968796ad55907b
KellyKimHyeJin <kelly317kim@naver.com>
2023-08-06 10:41:39 +0900

    feat: Add Exam Features

---
+++ src/main/java/com/khureturn/community/controller/ExamController.java
@@ -0,0 +1,35 @@
+package com.khureturn.community.controller;
+
+import com.khureturn.community.domain.exam.Exam;
+import com.khureturn.community.dto.ExamRequestDto;
+import com.khureturn.community.dto.ExamResponseDto;
+import com.khureturn.community.service.ExamService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/exam")
+public class ExamController {
+
+    private final ExamService examService;
+
+    @PostMapping("")
+    public ResponseEntity<ExamResponseDto.CreateExamDto> createExam(Principal principal, @RequestPart(value = "fileList", required= false) List<MultipartFile> fileList,
+                                                                    @RequestPart(value="data") ExamRequestDto.CreateExamDto data) throws IOException{
+        Exam exam = examService.create(fileList, data, principal);
+        return ResponseEntity.ok(ExamResponseDto.CreateExamDto.builder().examId(exam.getId()).build());
+    }
+
+    @PutMapping("/{examId}")
+    public ResponseEntity<ExamResponseDto.UpdateExamDto> updateExam(@PathVariable(name = "examId") Long examId, @RequestBody ExamRequestDto.UpdateExamDto request){
+        Exam exam = examService.update(examId, request);
+        return ResponseEntity.ok(ExamResponseDto.UpdateExamDto.builder().examId(exam.getId()).build());
+    }
+}
---
+++ src/main/java/com/khureturn/community/service/ExamService.java
@@ -0,0 +1,63 @@
+package com.khureturn.community.service;
+
+import com.khureturn.community.domain.Member;
+import com.khureturn.community.domain.exam.Exam;
+import com.khureturn.community.domain.exam.ExamFile;
+import com.khureturn.community.dto.ExamRequestDto;
+import com.khureturn.community.exception.NotFoundException;
+import com.khureturn.community.repository.ExamFileRepository;
+import com.khureturn.community.repository.ExamRepository;
+import com.khureturn.community.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.security.Principal;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ExamService {
+
+    private final ExamRepository examRepository;
+
+    private final MemberRepository memberRepository;
+    private final ExamFileRepository examFileRepository;
+
+    @Transactional
+    public Exam create(List<MultipartFile> fileList, ExamRequestDto.CreateExamDto request, Principal principal) throws IOException {
+        Member member = memberRepository.findByName(principal.getName());
+        Exam exam = Exam.builder()
+                .examTitle(request.getTitle())
+                .examContent(request.getContent())
+                .isAnonymous(request.getIsAnonymous())
+                .member(member)
+                .build();
+        examRepository.save(exam);
+
+        if(fileList != null){
+            for(MultipartFile file: fileList){
+                ExamFile examFile = ExamFile.builder()
+                        .examFileUrl(ExamFileService.fileUpload(file))
+                        .exam(exam)
+                        .build();
+                examFileRepository.save(examFile);
+
+            }
+        }
+        return exam;
+    }
+
+    @Transactional
+    public Exam update(Long examId, ExamRequestDto.UpdateExamDto request){
+        Exam exam = examRepository.findById(examId)
+                .orElseThrow(()-> new NotFoundException("족보를 찾을 수 없습니다."));
+        exam.update(request.getTitle(), request.getContent(), request.getIsAnonymous());
+        return exam;
+    }
+
+
+
+}
