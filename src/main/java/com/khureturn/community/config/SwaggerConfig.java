15a97f83867ff9295df7f265a4fb4478440be62b
choihyunyeong <62254434+Martin0o0@users.noreply.github.com>
2023-08-13 14:48:26 +0900

    hotfix: change swagger server url (#65)

--- src/main/java/com/khureturn/community/config/SwaggerConfig.java
+++ src/main/java/com/khureturn/community/config/SwaggerConfig.java
@@ -3,8 +3,13 @@ package com.khureturn.community.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.boot.autoconfigure.rsocket.RSocketProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Configuration
@@ -12,9 +17,13 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI(){
+        Server server = new Server();
+        server.setUrl("https://khu-return.site");
+        List<Server> servers = new ArrayList<>();
+        servers.add(server);
         return new OpenAPI()
-                .components(new Components())
-                .info(apiInfo());
+                .components(new Components()).servers(servers).info(apiInfo());
+
     }
 
     private Info apiInfo() {
